4f8d59eb3c6e38e4ac552f04c2b9560b90f2a80a
Ashutosh Sanzgiri <sanzgiri@gmail.com>
2026-02-05 22:33:39 +0000

    auto-save #71: 2026-02-05 22:33:39

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Allow dev server access from local network
+  allowedDevOrigins: ['http://10.0.0.161:3000', 'http://localhost:3000'],
 };
 
 export default nextConfig;
